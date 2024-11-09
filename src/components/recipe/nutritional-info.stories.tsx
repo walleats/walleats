8f4435d92c47e926ec27e551a8df175bb5453949
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-11-09 14:13:38 -0300

    add nutritional info and nutritional properties

---
+++ src/components/recipe/nutritional-info.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { NutritionalInfo, NutritionalInfoProps } from './NutritionalInfo';
+import { icons } from '@/constants/icons';
+
+
+const nutritionalProperties = [
+    { iconPath: icons.fire, propertyValue: '200 kcal' },
+    { iconPath: icons.food, propertyValue: '10 g fat' },
+    { iconPath: icons.fire, propertyValue: '150 kcal' },
+    { iconPath: icons.food, propertyValue: '8 g fat' },
+    { iconPath: icons.fire, propertyValue: '250 kcal' },
+    { iconPath: icons.food, propertyValue: '12 g fat' },
+];
+
+const meta: Meta<NutritionalInfoProps> = {
+    title: 'NutritionalInfo',
+    component: NutritionalInfo,
+    parameters: { layout: 'centered' },
+};
+
+export default meta;
+
+type Story = StoryObj<NutritionalInfoProps>;
+
+const Template = (args: NutritionalInfoProps) => <NutritionalInfo {...args} />;
+
+export const Default: Story = {
+    render: Template,
+    args: {
+        nutritionalProperties,
+    },
+};
