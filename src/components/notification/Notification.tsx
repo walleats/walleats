503599c4ec57db81cf38ed7e11b3f4fcfe253f7d
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-11-10 16:51:21 -0300

    hotfix

--- src/components/notification/Notification.tsx
+++ src/components/notification/Notification.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Icon } from "@/components/icon/Icon";
 import { icons } from "@/constants/icons";
-import {IconsProps} from "@storybook/core/components";
 
 export interface NotificationProps {
     variant?: 'success' | 'error' | 'warning';
     message: string;
 }
-
 
 export const Notification: React.FC<NotificationProps> = ({
                                                               message,
@@ -21,16 +19,21 @@ export const Notification: React.FC<NotificationProps> = ({
 
     const iconType = {
         success: icons.check,
-        error: icons.error,
+        error: icons.close,
         warning: icons.error
     };
 
+    const iconSize: { [key in 'success' | 'error' | 'warning']: 'small' | 'medium' | 'large' } = {
+        success: 'medium',
+        error: 'small',
+        warning: 'medium'
+    };
 
     return (
         <div className={`flex w-[404px] p-[10px] items-center gap-[10px] ${colorStyle[variant]}`}>
             <div className="flex justify-between items-center flex-[1_0_0]">
                 <div className="flex items-center gap-2.5 w-[248.5px]">
-                    <Icon icon={iconType[variant]} size="medium" />
+                    <Icon icon={iconType[variant]} size={iconSize[variant]} />
                     <span className="text-black font-poppins text-base font-medium leading-6">
                         {message}
                     </span>
