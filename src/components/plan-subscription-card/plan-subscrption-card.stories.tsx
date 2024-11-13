757d4568b38355058651755e70e4fe3e2d441974
MaxiRG <maxiramirezgis@gmail.com>
2024-11-13 01:19:17 -0300

    plan subscription card implemented

---
+++ src/components/plan-subscription-card/plan-subscrption-card.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { PlanSubscriptionCard, PlanSubscriptionCardProps } from './PlanSubscriptionCard';
+
+const meta: Meta<PlanSubscriptionCardProps> = {
+    title: 'PlanSubscriptionCard',
+    component: PlanSubscriptionCard,
+    parameters: { layout: 'centered' },
+};
+
+export default meta;
+type Story = StoryObj<PlanSubscriptionCardProps>;
+
+const Template = (args: PlanSubscriptionCardProps) => <PlanSubscriptionCard {...args} />;
+
+export const Default: Story = {
+    render: Template,
+    args: {
+        title: 'Daily Delight',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec dapibus odio.',
+        price: '$10.0',
+        label : 'Subscribe'
+    },
+};
