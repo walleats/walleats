f1964f49fe424e55a68d2854c434eb4239c1dcca
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2024-11-13 00:42:20 -0300

    add menu item component into story

---
+++ src/components/menu-item/menu-item.stories.tsx
@@ -0,0 +1,25 @@
+import {MenuItem, MenuItemProps} from "@/components/menu-item/MenuItem";
+import {Meta, StoryObj} from "@storybook/react";
+import {images} from "@/constants/images";
+
+const meta: Meta<MenuItemProps> = {
+    title: 'MenuItem',
+    component: MenuItem,
+    parameters: {layout: 'centered'},
+};
+
+export default meta;
+type Story = StoryObj<MenuItemProps>;
+
+const Template = (args: MenuItemProps) => <MenuItem {...args}/>
+
+
+export const Default: Story = {
+    render: Template,
+    args: {
+        productTitle: 'Some Title',
+        productDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae ornare nibh. Aenean vel pharetra quam.',
+        imagePath: images.mcdonalds,
+        price: '$20',
+    }
+}
